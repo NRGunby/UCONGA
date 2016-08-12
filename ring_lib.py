11390899d1800bb46c7fc21054e183c8d5b496f0
Nathaniel Gunby <nate.gunby@gmail.com>
2016-08-13 11:51:28 +1200

    Improve readibility of ring_lib.py

--- ring_lib.py
+++ ring_lib.py
@@ -9,7 +9,10 @@ from math import degrees
 def find_plane_of_ring(centered_ring_coords):
     '''
     Returns the normal to the plane of best fit of a ring system
-    Uses scipy for the linear algebra
+    Accepts:
+        A numpy array of the coordinates of the atoms in the ring
+    Returns:
+        A numpy array of the vector normal to the plane of the ring 
     '''
     p0 = [1.0, 1.0, 1.0, 1.0]
     def f_min(X, p):
@@ -26,13 +29,21 @@ def find_plane_of_ring(centered_ring_coords):
 def is_flippable(mol, ring_at_ids):
     '''
     Test if a ring is flippable
-    Rings which are noticeable non-planar are defined as being flippable
+    Rings which are noticeably non-planar are defined as being flippable
+    Takes:
+        A molecule object
+        A list of atom ids of the ring
+    Returns:
+        A boolean
     '''
+    # 3 points define a plane, and <4 atoms can't be non-planar
     if len(ring_at_ids) <= 3:
-        return False # 3 points define a plane, and <4 atoms can't be non-planar
+        return False
+    # Center the ring
     ring_coords = numpy.array([mol.atoms[i].coords for i in ring_at_ids])
     center_of_ring = numpy.average(ring_coords, axis=0)
     ring_coords -= center_of_ring
+    # Find the normal to the plane of the ring, and normalise it (set length to 1)
     with warnings.catch_warnings(record=True) as warnings_log:
         norm = linalg.normalise(find_plane_of_ring(ring_coords))
     if len([i for i in warnings_log if 'reached maxfev' in str(i.message)]):
@@ -53,8 +64,13 @@ def is_flippable(mol, ring_at_ids):
 def all_ring_conformers(mol, full_flip=2):
     '''
     Find all combinations of all ring conformers for all the rings in a molecule
+    Accepts:
+        A molecule object
+        An integer decribing the degree of ring conformer generation to undertake: none (0), partial (1), or full (2)
+    Returns:
+        A list of nomlecule objects, each with a different combination of ring conformers
     '''
-    all_ring_systems = find_ring_systems(mol) #Do we want to change this line?
+    all_ring_systems = find_ring_systems(mol)
     ring_conformer_combinations = [mol]
     for each_ring_system in all_ring_systems:
         if is_flippable(mol, each_ring_system):
@@ -67,8 +83,14 @@ def all_ring_conformers(mol, full_flip=2):
 def find_ring_conformers(base_mol, each_ring_system, full_flip=2):
     '''
     Finds all unique conformers for a given ring system
+    Accepts:
+        A molecule object
+        A list of atom ids of the ring
+        An integer decribing the degree of ring conformer generation to undertake: none (0), partial (1), or full (2)
+    Returns:
+        A list of molecule objects, each with a different combination of ring conformers
     '''
-    if full_flip > 1:
+    if full_flip > 1: # Full ring conformer generation
         ret = []
         found_conformers = []
         all_dihedrals = [i for i in base_mol.all_torsions()]
@@ -84,9 +106,9 @@ def find_ring_conformers(base_mol, each_ring_system, full_flip=2):
                     ret.append(mirror_image)
                     found_conformers.append(torsions)
             idx += 1
-    elif full_flip == 1:
+    elif full_flip == 1: # Partial ring conformer generation
         ret = [base_mol.copy(), flip_ring(base_mol, each_ring_system)]
-    else:
+    else: # No ring conformer generation
         ret = [base_mol.copy()]
     return ret
 
@@ -94,33 +116,44 @@ def find_ring_conformers(base_mol, each_ring_system, full_flip=2):
 def base_ring_variations(base_mol, each_ring_system):
     '''
     Finds all two-atom flips-of-fragments
-    The flip of fragments is defined in
-    Mekenyan et al, J. Chem. Inf. Model. 2005, p. 283
+    The flip of fragments is defined in Mekenyan et al, J. Chem. Inf. Model. 2005, p. 283
+    Accepts:
+        A molecule object
+        A list of atom ids of the ring
+    Returns:
+        A list of molecule objects, each with a different combination of ring conformers
     '''
-    aligned_mol = base_mol.copy()
+    aligned_mol = base_mol.copy() # Don't overwrite the base molecule
+    # Center the molecule on the ring to be flipped
     ring_coords = numpy.array([aligned_mol.atoms[i].coords for i in each_ring_system])
     center_of_ring = numpy.average(ring_coords, axis=0)
     aligned_mol.translate(-1*center_of_ring)
+    # Align the ring to be flipped on the xy plane
     ring_coords = numpy.array([aligned_mol.atoms[i].coords for i in each_ring_system])
     norm = find_plane_of_ring(ring_coords)
     xy_align = linalg.rotation_from_axes(norm, numpy.array([0.0, 0.0, 1.0]))
     aligned_mol.rotate(xy_align)
+    # Build the list of flips of fragments
     ret = [aligned_mol.copy()]
     ring_coords = numpy.array([aligned_mol.atoms[i].coords for i in each_ring_system])
-    z_to_precision = [abs(int(round(100*i[2]))) for i in ring_coords]
+    z_to_precision = [abs(int(round(100*i[2]))) for i in ring_coords] # What is this line even doing?
     if len(set(z_to_precision)) > 1 and len(each_ring_system) > 4:
-        neighbour_pairs = [[frozenset([i,j]) for j in aligned_mol.atoms[i].get_bond_ids() if j in each_ring_system]
+        # Make a list of two-atom groups in the ring
+        tmp_neighbour_pairs = [[frozenset([i,j]) for j in aligned_mol.atoms[i].get_bond_ids() if j in each_ring_system]
                                      for i in each_ring_system]
-        neighbour_pairs = set([i for i in chain(*neighbour_pairs)])
+        neighbour_pairs = set([i for i in chain(*tmp_neighbour_pairs)])
         for each_pair_to_flip in neighbour_pairs:
-            each_pair_to_flip = list(each_pair_to_flip) # Need it to have a distinct order, which frozensets don't
+            # The pair needs a distinct order, which frozensets don't
+            each_pair_to_flip = list(each_pair_to_flip) 
             new_mol = aligned_mol.copy()
             junction = [i for i in chain(*[filter(lambda x: x in each_ring_system, new_mol.atoms[i].search_away_from(j))
                                            for i, j in zip(each_pair_to_flip, each_pair_to_flip[::-1])])]
+            # Don't flip the ring at a ring junction
             if len(junction) != 2:
-                break # Let's not do this at a ring junction
+                break
+            # Don't flip atoms with pi-bonding
             if base_mol.atoms[junction[0]].get_hybridisation() != 3 or base_mol.atoms[junction[1]].get_hybridisation() != 3:
-                break # I *think* only sp3 ring junction can flip. May not work so well if metals/hypervalent atoms/etc are invovled
+                break
             substituents = [i for i in chain(*[new_mol.atoms[i].all_neighbours_away_from(*each_ring_system)
                                                for i in each_pair_to_flip + junction])]
             atoms_reference = [i for i in
@@ -144,6 +177,11 @@ def flip_ring(base_mol, each_ring_system):
     '''
     Takes the mirror image of a ring skeleton while preserving stereocehmistry at all the individual ring atoms
     This includes flipping a chair or half-chair conformation
+    Accepts:
+        A molecule object
+        A list of atom ids of the ring
+    Returns:
+        A molecule object
     '''
     mirror_image = base_mol.copy()
     z_mirror = numpy.array([1.0, 1.0, -1.0])
@@ -164,6 +202,12 @@ def flip_substituents(mol, each_ring_system, at_id):
     Change the stereocehsmitry at atom <at_id> in molecule <mol>
     This is part of the flip-of-fragments operation of Mekenyan et al
     See Mekenyan et al, J. Chem. Inf. Model. 2005, p. 283
+    Accepts:
+        A molecule object
+        A list of atom ids of the ring
+        The atom id to be corrected
+    Returns:
+        Nothing, this is a destructive method
     '''
     ring_atom = mol.atoms[at_id]
     in_ring_neighbours = [mol.atoms[i] for i in filter(lambda x: x in each_ring_system,
