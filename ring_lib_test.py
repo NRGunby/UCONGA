04bf8addd6708bb8081cbcb851d97942fc9d6496
Nathaniel Gunby <nate.gunby@gmail.com>
2016-10-14 14:38:17 +1300

    Added test for find_plane_of_ring on planar ring

--- ring_lib_test.py
+++ ring_lib_test.py
@@ -4,6 +4,7 @@ import unittest
 from random import shuffle
 import warnings
 from UCONGA_analyse import calc_min_rmsd
+from numpy import dot
 
 idx_conformer_testing = 0
 
@@ -37,6 +38,17 @@ class TestFindConformers(unittest.TestCase):
         rs = ring_lib.find_ring_systems(ech_mol)[0]
         confs = ring_lib.find_ring_conformers(ech_mol, rs)
         self.assertEqual(len(confs), 2)
+
+class TestFindPlane(unittest.TestCase):
+
+    def test_planar_ring(self):
+        m = molecule.from_cml('test_molecules/benzene.cml')
+        m.center()
+        m_carbons = m.copy_without_H()[0]
+        m_norm = ring_lib.find_plane_of_ring(m_carbons.coord_matrix())
+        for each_atom in m_carbons.atoms:
+            self.assertAlmostEqual(dot(each_atom.coords, m_norm), 0, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
